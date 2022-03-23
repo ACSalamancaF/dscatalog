8ace4d91e2e6864b09d04c2359fc3251946e4153
Carlos Salamanca <acsalamancaf@hotmail.com>
2022-03-23 15:42:39 -0300

    Pagination

--- backend/src/main/java/com/acsalamancaf/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/acsalamancaf/dscatalog/services/CategoryService.java
@@ -9,6 +9,8 @@ import javax.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -71,6 +73,12 @@ public class CategoryService {
 		catch(DataIntegrityViolationException e){
 			throw new DataBaseException("Dabase exception");
 		}
+	}
+
+	@Transactional(readOnly = true)
+	public Page<CategoryDTO> findAllPage(PageRequest pageRequest) {
+		Page<Category> list = categoryRepository.findAll(pageRequest);
+		return list.map(x -> new CategoryDTO(x));
 	}
 	
 }
